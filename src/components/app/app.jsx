60bdcaedc3c1941b1306450e9565ad4f840bac8a
blckwltz <ruslan.breev@flocktory.com>
2020-06-13 14:57:28 +0300

    add prop types, add movie card component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Main from '../main/main.jsx';
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {movieName, movieGenre, releaseDate} = props;
+  const {movieName, movieGenre, releaseDate, moviesNames} = props;
 
   return <Main
     name={movieName}
     genre={movieGenre}
     date={releaseDate}
+    moviesNames={moviesNames}
   />;
+};
+
+App.propTypes = {
+  movieName: PropTypes.string.isRequired,
+  movieGenre: PropTypes.string.isRequired,
+  releaseDate: PropTypes.number.isRequired,
+  moviesNames: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default App;
